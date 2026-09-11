414957b2b54a0a5764b8591ef1cf715d0e3dc5d0
Mammoman <daniel08seun@gmail.com>
2026-09-11 23:47:45 +0100

    flutter

--- ashiri-ecommerce/src/components/CartSidebar.jsx
+++ ashiri-ecommerce/src/components/CartSidebar.jsx
@@ -201,6 +201,7 @@ const CartSidebar = ({
             tx_ref: paymentRef,
             amount: calculateSubtotal(), // Flutterwave is in Naira
             currency: 'NGN',
+            payment_options: 'card, banktransfer, ussd',
             customer: {
               email: customerEmail,
               phone_number: customerPhone,
